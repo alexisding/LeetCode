699467fb67a34cc82a4f0e33ae4f0985df60a2b5
Zihan Alexis Ding <zihanalexisd@gmail.com>
2018-10-12 13:43:36 -0700

    added descriptions

--- src/ReverseStrings.java
+++ src/ReverseStrings.java
@@ -1,3 +1,17 @@
+/*
+344.
+Write a function that takes a string as input and returns the string reversed.
+
+Example 1:
+
+Input: "hello"
+Output: "olleh"
+Example 2:
+
+Input: "A man, a plan, a canal: Panama"
+Output: "amanaP :lanac a ,nalp a ,nam A"
+
+ */
 package com.company;
 
 import java.util.ArrayList;
